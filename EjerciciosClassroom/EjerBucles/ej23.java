fe5a5f15c08f53d07209c70ccce36607e41000bf
jorge1727 <jorge1727.jt@gmail.com>
2022-11-15 01:37:29 +0100

    casa correeccion de algunes EjerBucles

--- EjerciciosClassroom/EjerBucles/ej23.java
+++ EjerciciosClassroom/EjerBucles/ej23.java
@@ -6,21 +6,28 @@ public class ej23
     {
         int cont = 0;
         int sumaNum = 0;
+        int media = 0;
 
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("Introduce numero");
+        int num = sc.nextInt();
+        sumaNum = num;
+
         while(sumaNum <= 10000)
         {
             System.out.println("Introduce numero");
-            int num = sc.nextInt();
-
+            num = sc.nextInt();
+            
             sumaNum += num;
             cont++;
         }
         sc.close();
 
-        int media = (sumaNum / cont);
-
+        if(cont == 0)
+            media = sumaNum;
+        else
+            media = (sumaNum / cont);
+        
         System.out.println("El total acumulado es de: " + sumaNum + "\nEl contador es de: " + cont + "\nY la media es de: " + media);
     }    
 }
