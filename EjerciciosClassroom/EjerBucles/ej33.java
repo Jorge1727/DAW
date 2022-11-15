f54dfe4a8c5518de0d891453223f9ba44ee65a7d
jorge1727 <jorge1727.jt@gmail.com>
2022-11-16 00:41:38 +0100

    fin de ejercicios obligatorios de bucles

--- EjerciciosClassroom/EjerBucles/ej33.java
+++ EjerciciosClassroom/EjerBucles/ej33.java
@@ -1,7 +1,39 @@
+import java.util.Scanner;
+
 public class ej33 
 {
     public static void main(String[] args) 
     {
-        
+        int planta = 1;
+
+        System.out.println("Introduce la altura:");
+        Scanner sc = new Scanner(System.in);
+        int altura = sc.nextInt();
+        sc.close();
+
+        while(planta <= (altura - 1))
+        {
+            for(int i = 1; i <= altura; i++)
+            {
+                if((i >= 2) && (i <= (altura - 1)))
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            System.out.println();
+            planta++;
+        }
+
+        if(planta == altura)
+        {
+            for(int i = 1; i <= altura; i++)
+            {
+                if((i >= 2) && (i <= (altura - 1)))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
     }    
 }
